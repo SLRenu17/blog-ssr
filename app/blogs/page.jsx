99cd87a1eb7e6dce3a50a7809106a4f0c76ef01a
Avupati Ganesh <161594924+Ganeshavupati01@users.noreply.github.com>
2025-12-29 13:02:52 +0530

    Update page.jsx

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -60,7 +60,6 @@ export default async function BlogsPage() {
                 dangerouslySetInnerHTML={{ __html: post.title }} 
                 style={{ fontSize: '1.5rem', marginBottom: '1rem' }}
               />
-              <p style={{ color: '#666', marginBottom: '1rem' }}>{excerpt}</p>
               <Link href={`/blogs/${post.slug}`} style={{
                 color: '#2563eb',
                 textDecoration: 'none',
