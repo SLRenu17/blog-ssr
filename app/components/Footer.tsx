09dd8854e06c635072722492a5c8fa8ee4f47e58
Avupati Ganesh <161594924+Ganeshavupati01@users.noreply.github.com>
2025-12-29 13:37:31 +0530

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
-import "./footer.css";
+import "../footer.css";
 
 const Footer = () => {
   const socialIcons = [
