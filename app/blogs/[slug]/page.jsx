1f4e37c3cc6f0a3eec1b465d9546758b5d3f77a0
Avupati Ganesh <161594924+Ganeshavupati01@users.noreply.github.com>
2025-12-30 12:36:40 +0530

    Update page.jsx

--- app/blogs/[slug]/page.jsx
+++ app/blogs/[slug]/page.jsx
@@ -29,7 +29,7 @@ export default async function BlogDetails({ params }) {
         backgroundColor: 'white',
         padding: '2rem',
         borderRadius: '8px',
-        marginTop:'60px',
+        marginTop:'40px',
         boxShadow: '0 1px 3px rgba(0,0,0,0.1)'
       }}>
         <h1 
